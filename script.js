68d386e380fd2f16e98ddc609d5f754ddbb45068
klauder-toby <tklauder21@georgefox.edu>
2021-11-06 18:32:01 -0700

    the backbone of our fine economy

---
+++ script.js
@@ -0,0 +1,148 @@
+var can = document.getElementById("myCanvas"); 
+var ctx = can.getContext("2d");
+ctx.fillStyle = "#CCCCCC"; 
+can.addEventListener("click", canvasClicked);
+/*Square should also be a class with attributes of status (blank, X, or O) and location. It should also have a draw method. */
+
+class Location {
+  constructor(x, y) {
+    this.x = x; 
+    this.y = y; 
+  }
+}
+
+class Canvas {
+  constructor(squares) {
+    this.squares = squares; 
+
+    for(var i in squares) {
+      console.log(i);
+      squares[i].draw(); 
+    }
+
+    ctx.beginPath();
+    ctx.moveTo(100, 0); 
+    ctx.lineTo(100, 300);
+    ctx.closePath(); 
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "black";
+    ctx.stroke(); 
+
+    ctx.beginPath();
+    ctx.moveTo(200, 0); 
+    ctx.lineTo(200, 300);
+    ctx.closePath(); 
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "black";
+    ctx.stroke(); 
+
+    ctx.beginPath();
+    ctx.moveTo(0, 100); 
+    ctx.lineTo(300, 100);
+    ctx.closePath(); 
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "black";
+    ctx.stroke(); 
+
+    ctx.beginPath();
+    ctx.moveTo(0, 200); 
+    ctx.lineTo(300,200);
+    ctx.closePath(); 
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "black";
+    ctx.stroke(); 
+  }
+}
+function canvasClicked(e) {
+   // Get the canvas and bounding client rectangle
+   const canvas = e.target;
+   const rect = canvas.getBoundingClientRect();
+   
+   // Compute click coordinates, relative to the canvas
+   const x = e.clientX - rect.left;
+   const y = e.clientY - rect.top;
+   
+   // Convert from pixel coordinates to row, column
+   const row = Math.floor(y * 3 / canvas.height);
+   const column = Math.floor(x * 3 / canvas.width);
+   
+   // Call clickLight
+   clickSquare(row, column);
+}
+
+
+class Square {
+  constructor(status, location) {
+    this.status = status; 
+    this.location = location; 
+  }
+  
+   draw() {
+       ctx.fillRect(this.location.x * 100, this.location.y * 100, 100, 100);
+   }
+  /*get status_() {
+    return this.status;
+  }
+
+  set status_(newStatus) {
+    this.status = newStatus;
+  }
+
+  get location_() {
+    return this.location; 
+  }
+  set location_(newLocation) {
+    this.location = newLocation;
+  }
+  */
+}
+
+class Player {
+  constructor(name, isCurrent, winner) {
+    this.name = name; 
+    this.isCurrent = isCurrent; 
+    this.winner = winner; 
+  }
+  /*
+  get name() {
+    return this.name; 
+  }
+  set name(newName) {
+    this.name = newName;
+  }
+  get isCurrent() {
+    return this.isCurrent;
+  }
+  set isCurrent(newStatus) {
+    this.isCurrent = isCurrent; 
+  }
+  get winner() {
+    return this.winner;
+  }
+  set winner(newWinner){
+    this.winner = newWinner; 
+  }
+  */
+}
+
+let player = new Player("Toby", false, false);
+let player1 = new Player("Dylan", true, false);
+let square0 = new Square("blank", new Location(0, 0));
+let square1 = new Square("blank", new Location(0, 1));
+let square2 = new Square("blank", new Location(0, 2));
+let square3 = new Square("blank", new Location(1, 0));
+let square4 = new Square("blank", new Location(1, 1));
+let square5 = new Square("blank", new Location(1, 2));
+let square6 = new Square("blank", new Location(2, 0));
+let square7 = new Square("blank", new Location(2, 1));
+let square8 = new Square("blank", new Location(2, 2));
+let squares = [square0, square1, square2, square3, square4, square5, square6, square7, square8];
+let c = new Canvas(squares);
+
+function clickSquare(r, c) {
+ for(var i in squares) {
+   if(squares[i].location.x == r && squares[i].location.y == c) {
+     squares[i].draw(); 
+   }
+ } 
+}
