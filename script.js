d549442a4d561c9657a517fc97abba90c3ecec6a
klauder-toby <tklauder21@georgefox.edu>
2021-11-11 12:31:07 -0800

    actually patch

--- script.js
+++ script.js
@@ -215,15 +215,15 @@ function draw() {
     if(y + dy < ballRadius) {
         dy = -dy;
     } //ball bounce off paddle check 
-    else if(y + dy > canvas.height) {
+    else if(y + dy > canvas.height-ballRadius - paddleHeight) {
         if(x > paddleX && x < paddleX + paddleWidth) {
             dy = -dy;
         }
-        else {
-            alert("Game over - Hit enter to play again. ");
-            document.location.reload();
-            clearInterval(interval); 
-        }
+    }
+    if (y + dy > canvas.height) {
+        alert("Game over - Hit enter to play again. ");
+        document.location.reload();
+        clearInterval(interval); 
     }
     
     if(rightPressed && paddleX < canvas.width-paddleWidth) {
